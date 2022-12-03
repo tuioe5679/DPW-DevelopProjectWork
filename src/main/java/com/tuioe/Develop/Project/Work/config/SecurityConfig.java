7f895c28e9243ac395b49e18ea7f248aab910907
tuioe5679 <qodlgks123@naver.com>
2022-12-03 21:12:35 +0900

    refactor: Security 설정

--- src/main/java/com/tuioe/Develop/Project/Work/config/SecurityConfig.java
+++ src/main/java/com/tuioe/Develop/Project/Work/config/SecurityConfig.java
@@ -1,20 +1,28 @@
 package com.tuioe.Develop.Project.Work.config;
 
 import com.tuioe.Develop.Project.Work.auth.CustomOAuth2UserService;
-import com.tuioe.Develop.Project.Work.domain.user.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnDefaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
@@ -22,18 +30,15 @@ public class SecurityConfig {
                 .and()
                     .authorizeRequests()
                     .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").permitAll()
-                    .antMatchers("/api/**").hasRole(Role.USER.name())
                     .anyRequest().permitAll()
                 .and()
                     .logout()
                     .logoutSuccessUrl("/")
                 .and()
                     .oauth2Login()
-                    .redirectionEndpoint()
-                    .baseUri("/login/oauth2/code/github")
-                .and()
                     .userInfoEndpoint()
                     .userService(customOAuth2UserService);
+
         return http.build();
     }
 }
