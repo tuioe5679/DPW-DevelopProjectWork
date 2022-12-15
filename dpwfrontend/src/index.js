0c012861d14f0e6312964a9e4dd7c45134377648
tuioe5679 <qodlgks123@naver.com>
2022-12-16 01:40:57 +0900

    페이지 라우터 추가

--- dpwfrontend/src/index.js
+++ dpwfrontend/src/index.js
@@ -5,7 +5,9 @@ import ProjectDetail from "./page/Project/ProjectDetail";
 import ProjectUpdate from "./page/Project/ProjectUpdate";
 import DevelopDiaryWrite from "./page/Develop/DevelopDiaryWrite";
 import DevelopDiaryList from "./page/Develop/DevelopDiaryList";
+import DevelopDiaryUpdate from "./page/Develop/DevelopDiaryUpdate";
 import Main from "./page/Main";
+import DevelopDiaryDetail from "./page/Develop/DevelopDiaryDetail";
 
 const rootElement = document.getElementById("root");
 render(
@@ -14,8 +16,10 @@ render(
       <Route path="/" element={<Main />} />
       <Route path="write" element={<ProjectWrite />} />
       <Route path="project/:id" element={<ProjectDetail />} />
-      <Route path="DevelopDiaryWrite/:id" element={<DevelopDiaryWrite />} />
+      <Route path="developDiaryWrite/:id" element={<DevelopDiaryWrite />} />
       <Route path="developDiaryList/:id" element={<DevelopDiaryList />} />
+      <Route path="developDiaryUpdate/:id" element={<DevelopDiaryUpdate />} />
+      <Route path="developDiaryDeatil/:id" element={<DevelopDiaryDetail />} />
       <Route path="projectUpdate/:id" element={<ProjectUpdate />} />
     </Routes>
   </BrowserRouter>,
