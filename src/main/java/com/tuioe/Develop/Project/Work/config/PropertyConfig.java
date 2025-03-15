043664ba507195226c10d382af259a0049352044
tuioe <qodlgks123@naver.com>
2025-03-16 00:16:03 +0900

    feat: 민감 정보 env.properties 파일 관리

---
+++ src/main/java/com/tuioe/Develop/Project/Work/config/PropertyConfig.java
@@ -0,0 +1,12 @@
+package com.tuioe.Develop.Project.Work.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:properties/env.properties")
+})
+public class PropertyConfig {
+}
